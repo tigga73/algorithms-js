337418a0c00a71a2e59b82eb608a54a56b542eb2
Tiago Silva Cardoso - 06453943514 <tiago.s.cardoso@animaeducacao.com.br>
2023-10-25 12:16:56 -0300

    recursive factorial of a number

---
+++ 6-recursive-factorial-of-number/index.js
@@ -0,0 +1,11 @@
+// Big-O = O(n)
+const recursiveFactorial = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
+};
+
+console.log(recursiveFactorial(0)); // 1
+console.log(recursiveFactorial(1)); // 1
+console.log(recursiveFactorial(5)); // 120
